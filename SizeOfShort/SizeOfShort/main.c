881e89af86fa720cec317896305115ae32ecc5b3
Raymond Chua <ray.r.chua@gmail.com>
2014-08-04 11:49:09 +0800

    Sync to latest revision
    Sync to latest revision

---
+++ SizeOfShort/SizeOfShort/main.c
@@ -0,0 +1,19 @@
+//
+//  main.c
+//  SizeOfShort
+//
+//  Created by bitsandatoms on 28/6/14.
+//  Copyright (c) 2014 bitsandatoms. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    short s = 0;
+    printf("Size of Short: %zu bytes.\n", sizeof(s));
+    return 0;
+}
+
